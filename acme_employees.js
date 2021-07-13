a0a29d587487c5508b8a59f4e5a511c85cf1a221
Stephen Wong <stephen_wong_tx@icloud.com>
2021-07-13 13:25:04 -0500

    saving changes

--- acme_employees.js
+++ acme_employees.js
@@ -27,4 +27,44 @@ const findCoworkersFor = (func, array) => {
 }
 
 
-console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));
+// console.log(findCoworkersFor(findEmployeeByName('larry', employees), employees));
+
+
+//given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager 
+// console.log(findManagementChainForEmployee(findEmployeeByName('moe', employees), employees));//[  ]
+
+// console.log(findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees));/*
+// [ { id: 1, name: 'moe' },
+//   { id: 2, name: 'larry', managerId: 1 },
+//   { id: 4, name: 'shep', managerId: 2 }]
+
+const findManagementChainForEmployee = (func, array) => {
+  console.log('func: ', func);
+
+  let result = [];
+
+  if (func['managerId'] === undefined) return func;
+  let manager = findManagerFor(func, array);
+
+  result.push({...manager});
+  console.log('result', result);
+  return result.push(findManagementChainForEmployee(manager, array));
+}
+console.log('final result: ', findManagementChainForEmployee(findEmployeeByName('shep Jr.', employees), employees))
+// const findManagementChainForEmployee = (func, array) => {
+//   let result = [];
+//   let manager = findManagerFor(func, array);
+//   if (manager === undefined) {
+//     return result;
+//   }
+//   else {
+//     result.push({...manager});
+//     let chain = array.reduce((chain, employee) => {
+//       let pointer = result[0];
+//       if (employee === pointer) {
+//         chain.unshift(findManagerFor(pointer, array));
+//       }
+//       return chain;  
+//     }, result)
+//   }
+// }
